10b8d83adb99a7b27950312c19395ee49caf94ce
Kensuke Kubo <kensukekubo19@gmail.com>
2020-09-19 23:47:16 +0900

    chshをrootで

--- etc/initialize.sh
+++ etc/initialize.sh
@@ -59,7 +59,7 @@ fi
 brew install fish
 
 sudo bash -c "echo '/usr/local/bin/fish' >> /etc/shells"
-chsh -s /usr/local/bin/fish
+sudo chsh -s /usr/local/bin/fish
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
