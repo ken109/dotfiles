36e5e7f6fddf88e4bb24debe2e9f25ab7919d7c2
Kensuke Kubo <kensukekubo19@gmail.com>
2020-09-20 01:47:52 +0900

    configの中身を全て移し直す

--- etc/install.sh
+++ etc/install.sh
@@ -18,128 +18,12 @@ _SPACE_=' '
 _BLANK_="${_SPACE_}${_TAB_}"
 _IFS_="$IFS"
 
-vitalize() {
-    return 0
-}
-
-# Gerenal utilities {{{1
-is_interactive() {
-    if [ "${-/i/}" != "$-" ]; then
-        return 0
-    fi
-    return 1
-}
-
-is_bash() {
-    [ -n "$BASH_VERSION" ]
-}
-
-is_zsh() {
-    [ -n "$ZSH_VERSION" ]
-}
-
-is_at_least() {
-    if [ -z "$1" ]; then
-        return 1
-    fi
-
-    # For Z shell
-    if is_zsh; then
-        autoload -Uz is-at-least
-        is-at-least "${1:-}"
-        return $?
-    fi
-
-    atleast="$(echo $1 | sed -e 's/\.//g')"
-    version="$(echo ${BASH_VERSION:-0.0.0} | sed -e 's/^\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\).*/\1/' | sed -e 's/\.//g')"
-
-    # zero padding
-    while [ ${#atleast} -ne 6 ]; do
-        atleast="${atleast}0"
-    done
-
-    # zero padding
-    while [ ${#version} -ne 6 ]; do
-        version="${version}0"
-    done
-
-    # verbose
-    #echo "$atleast < $version"
-    if [ "$atleast" -le "$version" ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# ostype returns the lowercase OS name
-ostype() {
-    # shellcheck disable=SC2119
-    uname | lower
-}
-
-# os_detect export the PLATFORM variable as you see fit
-os_detect() {
-    export PLATFORM
-    case "$(ostype)" in
-    *'linux'*) PLATFORM='linux' ;;
-    *'darwin'*) PLATFORM='osx' ;;
-    *'bsd'*) PLATFORM='bsd' ;;
-    *) PLATFORM='unknown' ;;
-    esac
-}
-
-# is_osx returns true if running OS is Macintosh
-is_osx() {
-    os_detect
-    if [ "$PLATFORM" = "osx" ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-alias is_mac=is_osx
-
-# is_linux returns true if running OS is GNU/Linux
-is_linux() {
-    os_detect
-    if [ "$PLATFORM" = "linux" ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# is_bsd returns true if running OS is FreeBSD
-is_bsd() {
-    os_detect
-    if [ "$PLATFORM" = "bsd" ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# get_os returns OS name of the platform that is running
-get_os() {
-    local os
-    for os in osx linux bsd; do
-        if is_$os; then
-            echo $os
-        fi
-    done
-}
-
 e_newline() {
     printf "\n"
 }
 
 e_header() {
     printf " \033[37;1m%s\033[m\n" "$*"
-}
-
-e_error() {
-    printf " \033[31m%s\033[m\n" "✖ $*" 1>&2
 }
 
 e_warning() {
@@ -152,25 +36,6 @@ e_done() {
 
 e_arrow() {
     printf " \033[37;1m%s\033[m\n" "➜ $*"
-}
-
-e_indent() {
-    for ((i = 0; i < ${1:-4}; i++)); do
-        echon " "
-    done
-    if [ -n "$2" ]; then
-        echo "$2"
-    else
-        cat <&0
-    fi
-}
-
-e_success() {
-    printf " \033[37;1m%s\033[m%s...\033[32mOK\033[m\n" "✔ " "$*"
-}
-
-e_failure() {
-    die "${1:-$FUNCNAME}"
 }
 
 ink() {
@@ -248,10 +113,6 @@ logging() {
     echo
 }
 
-log_pass() {
-    logging SUCCESS "$1"
-}
-
 log_fail() {
     logging ERROR "$1" 1>&2
 }
@@ -260,66 +121,10 @@ log_fail() {
     logging WARN "$1"
 }
 
-log_info() {
-    logging INFO "$1"
-}
-
-log_echo() {
-    logging TITLE "$1"
-}
-
 # is_exists returns true if executable $1 exists in $PATH
 is_exists() {
     which "$1" >/dev/null 2>&1
     return $?
-}
-
-# has is wrapper function
-has() {
-    is_exists "$@"
-}
-
-# die returns exit code error and echo error message
-die() {
-    e_error "$1" 1>&2
-    exit "${2:-1}"
-}
-
-# is_login_shell returns true if current shell is first shell
-is_login_shell() {
-    [ "$SHLVL" = 1 ]
-}
-
-# is_git_repo returns true if cwd is in git repository
-is_git_repo() {
-    git rev-parse --is-inside-work-tree &>/dev/null
-    return $?
-}
-
-# is_screen_running returns true if GNU screen is running
-is_screen_running() {
-    [ ! -z "$STY" ]
-}
-
-# is_tmux_runnning returns true if tmux is running
-is_tmux_runnning() {
-    [ ! -z "$TMUX" ]
-}
-
-# is_screen_or_tmux_running returns true if GNU screen or tmux is running
-is_screen_or_tmux_running() {
-    is_screen_running || is_tmux_runnning
-}
-
-# shell_has_started_interactively returns true if the current shell is
-# running from command line
-shell_has_started_interactively() {
-    [ ! -z "$PS1" ]
-}
-
-# is_ssh_running returns true if the ssh deamon is available
-is_ssh_running() {
-    [ ! -z "$SSH_CLIENT" ]
 }
 
 # is_debug returns true if $DEBUG is set
@@ -329,125 +134,6 @@ is_debug() {
     else
         return 1
     fi
-}
-
-# is_number returns true if $1 is int type
-is_number() {
-    if [ $# -eq 0 ]; then
-        cat <&0
-    else
-        echo "$1"
-    fi | grep -E '^[0-9]+$' >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-alias is_int=is_number
-alias is_num=is_number
-
-# echon is a script to emulate the -n flag functionality with 'echo'
-# for Unix systems that don't have that available.
-echon() {
-    echo "$*" | tr -d '\n'
-}
-
-# noecho is the same as echon
-noecho() {
-    if [ "$(echo -n)" = "-n" ]; then
-        echo "${*:-> }\c"
-    else
-        echo -n "${@:-> }"
-    fi
-}
-
-# lower returns a copy of the string with all letters mapped to their lower case.
-# shellcheck disable=SC2120
-lower() {
-    if [ $# -eq 0 ]; then
-        cat <&0
-    elif [ $# -eq 1 ]; then
-        if [ -f "$1" -a -r "$1" ]; then
-            cat "$1"
-        else
-            echo "$1"
-        fi
-    else
-        return 1
-    fi | tr "[:upper:]" "[:lower:]"
-}
-
-# upper returns a copy of the string with all letters mapped to their upper case.
-# shellcheck disable=SC2120
-upper() {
-    if [ $# -eq 0 ]; then
-        cat <&0
-    elif [ $# -eq 1 ]; then
-        if [ -f "$1" -a -r "$1" ]; then
-            cat "$1"
-        else
-            echo "$1"
-        fi
-    else
-        return 1
-    fi | tr "[:lower:]" "[:upper:]"
-}
-
-# contains returns true if the specified string contains
-# the specified substring, otherwise returns false
-# http://stackoverflow.com/questions/2829613/how-do-you-tell-if-a-string-contains-another-string-in-unix-shell-scripting
-contains() {
-    string="$1"
-    substring="$2"
-    if [ "${string#*$substring}" != "$string" ]; then
-        return 0 # $substring is in $string
-    else
-        return 1 # $substring is not in $string
-    fi
-}
-
-# len returns the length of $1
-len() {
-    local length
-    length="$(echo "$1" | wc -c | sed -e 's/ *//')"
-    #echo "$(expr "$length" - 1)"
-    echo $(("$length" - 1))
-}
-
-# is_empty returns true if $1 consists of $_BLANK_
-is_empty() {
-    if [ $# -eq 0 ]; then
-        cat <&0
-    else
-        echo "$1"
-    fi | grep -E "^[$_BLANK_]*$" >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# path_remove returns new $PATH trailing $1 in $PATH removed
-# It was heavily inspired by http://stackoverflow.com/a/2108540/142339
-path_remove() {
-    if [ $# -eq 0 ]; then
-        die "too few arguments"
-    fi
-
-    local arg path
-
-    path=":$PATH:"
-
-    for arg in "$@"; do
-        path="${path//:$arg:/:}"
-    done
-
-    path="${path%:}"
-    path="${path#:}"
-
-    echo "$path"
 }
 
 # Dotfiles {{{1
@@ -533,9 +219,15 @@ dotfiles_deploy() {
         :
     else
         if [ -d "$HOME/.config" ]; then
-            mv -f ~/.config ~/.config.bk
+            mv -f ~/.config ~/.config.tmp
         fi
+
         make deploy
+
+        if [ -d "$HOME/.config.tmp" ]; then
+            mv ~/.config.tmp/* ~/.config/
+            rm -rf ~/.config.tmp
+        fi
     fi &&
         e_newline && e_done "Deploy"
 }
