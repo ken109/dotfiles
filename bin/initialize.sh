799435c1c75348895f11e6a9ee1753ca6beb875e
Kensuke Kubo <kensukekubo19@gmail.com>
2021-08-12 20:58:14 +0900

    fix: dependency install order

--- bin/initialize.sh
+++ bin/initialize.sh
@@ -11,38 +11,39 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 source "$HOME/.asdf/asdf.sh"
 
 if [ "$(uname)" = "Darwin" ]; then
-  brew install zsh tmux neovim git fzf exa bat ripgrep ghq fd
-
-  brew install trash git-flow ken109/tap/lcl gpg gawk
+  # nodejs
+  brew install gpg gawk
 
   # php build
   brew install autoconf automake bison freetype gd gettext icu4c krb5 libedit libiconv libjpeg libpng libxml2 libzip pkg-config re2c zlib
 
   brew tap homebrew/cask-fonts
-
   brew install --cask font-hack-nerd-font
+
+  brew install trash git-flow ken109/tap/lcl
+  brew install zsh tmux neovim git fzf exa bat ripgrep ghq fd
 
   sudo bash -c "echo '$(brew --prefix)/bin/zsh' >> /etc/shells"
   sudo chsh -s "$(brew --prefix)/bin/zsh" "$USER"
 
   "$(brew --prefix)/opt/fzf/install"
 elif [ "$(uname)" = "Linux" ]; then
-  asdf plugin add golang
-  go_version=$(asdf list all golang | rg '^[0-9]+\.[0-9]+\.[0-9]+$' | tail -1)
-  asdf install golang "$go_version"
-  asdf global golang "$go_version"
-
-  go get github.com/x-motemen/ghq
-
   sudo apt update
-
-  sudo apt install -y zsh tmux neovim git fzf exa bat ripgrep fd-find
 
   # nodejs
   sudo apt install -y dirmngr gpg curl gawk
 
   # php build
   sudo apt install -y autoconf bison build-essential curl gettext git libgd-dev libcurl4-openssl-dev libedit-dev libicu-dev libjpeg-dev libmysqlclient-dev libonig-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libzip-dev openssl pkg-config re2c zlib1g-dev
+
+  sudo apt install -y zsh tmux neovim git fzf exa bat ripgrep fd-find
+
+  asdf plugin add golang
+  go_version=$(asdf list all golang | rg '^[0-9]+\.[0-9]+\.[0-9]+$' | tail -1)
+  asdf install golang "$go_version"
+  asdf global golang "$go_version"
+
+  go get github.com/x-motemen/ghq
 
   sudo chsh -s "/usr/bin/zsh" "$USER"
 fi
